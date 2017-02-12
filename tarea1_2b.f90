708938e63cfd52d1dea75b1c425d624e8f69d177
david <ager_18@hotmail.com>
2017-02-11 23:58:43 -0600

    ejercicio completo

--- tarea1_2b.f90
+++ tarea1_2b.f90
@@ -1,12 +1,11 @@
-program montecarlo
+program condicion
 
 ntotal=1000000
-r=rand(129)
+n=rand(120)
 do i=0,ntotal
 x=rand(0)
+if (x.gt.0.30.and.x.lt.0.55) nr=nr+1
 write(*,*) x
-if (x.lt.0.25) nr=nr+1
 enddo
 write(*,*) nr, 100*float(nr)/float(ntotal)
 end
-
